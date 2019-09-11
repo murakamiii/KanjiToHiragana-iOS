f457912ae127d71a1ae50583a3821963c9306e0d
murakami <9khc727ru7@gmail.com>
2019-09-11 14:28:17 +0900

    MainView対応

--- StringToHiraganaiOS/ViewController.swift
+++ StringToHiraganaiOS/ViewController.swift
@@ -7,11 +7,35 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var inputTextView: UITextView!
+    @IBOutlet private weak var translateButton: UIButton!
+    
+    var viewModel: MainViewModel!
+    let disposebag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        bindUI()
+    }
+    
+    private func bindUI() {
+        let input = inputTextView.rx
+            .text
+            .orEmpty
+            .asObservable()
+        let tapEvent = translateButton.rx.tap.asObservable()
+        
+        let vm = MainViewModel(textInput: input, buttonEvent: tapEvent, service: TranslateService())
+        
+        vm.converted.asObservable().subscribe(onNext: { str in
+            print(str)
+        }).disposed(by: disposebag)
+        
+        viewModel = vm
     }
 }
