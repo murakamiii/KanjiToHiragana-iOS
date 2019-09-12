937b4f2f055c719863073f94b9529263aadff5bc
murakami <9khc727ru7@gmail.com>
2019-09-12 13:05:47 +0900

    コード整理

--- StringToHiraganaiOS/MainViewController/MainViewModel.swift
+++ StringToHiraganaiOS/MainViewController/MainViewModel.swift
@@ -18,12 +18,13 @@ class MainViewModel {
     init(textInput: Observable<String>, buttonEvent: Observable<Void>, service: TranslateServiceProtocol) {
         let load = BehaviorRelay<Bool>(value: false)
         
-        let res = buttonEvent.debug().withLatestFrom(textInput)
+        let res = buttonEvent.withLatestFrom(textInput)
             .do(onNext: { _ in
                 load.accept(true)
             })
             .flatMapLatest {
-                service.translate(sentence: $0).materialize()
+                service.translate(sentence: $0)
+                    .materialize()
             }
             .do(onNext: { _ in
                 load.accept(false)
@@ -33,7 +34,6 @@ class MainViewModel {
         converted = res.elements()
         error = res.errors()
             .map { $0 as? APIError ?? APIError.unknown(error: $0) }
-        
         isLoading = load
     }
 }
